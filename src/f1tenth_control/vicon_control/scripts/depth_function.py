fb151e672d682089142df594001930557882907e
StanleyHGu <123708225+StanleyHGu@users.noreply.github.com>
2023-11-26 19:15:45 -0500

    Update depth_function.py

--- src/f1tenth_control/vicon_control/scripts/depth_function.py
+++ src/f1tenth_control/vicon_control/scripts/depth_function.py
@@ -17,7 +17,7 @@ def read_params(file_path):
 
 def calculate_depth_at_pixel(x, y, height, width, normal, camera_height, camera_matrix):
     if y < height // 2 or y >= height or x < 0 or x >= width:
-        return "Pixel out of range for depth calculation"
+        return 0
     ray = np.linalg.inv(camera_matrix) @ np.array([x, y, 1])
     cos_theta = np.dot(ray, normal) / (np.linalg.norm(ray) * np.linalg.norm(normal))
     depth = camera_height / cos_theta
@@ -26,3 +26,30 @@ def calculate_depth_at_pixel(x, y, height, width, normal, camera_height, camera_
 def get_depth_for_pixel(x, y, params_file):
     params = read_params(params_file)
     return calculate_depth_at_pixel(x, y, params['Image Height'], params['Image Width'], params['Ground Plane Normal'], params['Camera Height'], params['Camera Matrix'])
+
+
+def trans_Pix2Camera_v2( midpoint_x, midpoint_y, depth, params_file):
+    # Read parameters from the file
+    params = read_params(params_file)
+
+    # Extract intrinsic matrix values
+    fx = params['Camera Matrix'][0, 0]
+    fy = params['Camera Matrix'][1, 1]
+    cx = params['Camera Matrix'][0, 2]
+    cy = params['Camera Matrix'][1, 2]
+
+    # Calculate inverse of camera matrix
+    K_inv = np.linalg.inv(np.array([[fx, 0, cx],
+                                    [0, fy, cy],
+                                    [0, 0, 1]]))
+
+    # Pixel coordinates in homogeneous form
+    pixel_coords_homogeneous = np.array([midpoint_x, midpoint_y, 1])
+
+    # Camera local coordinates
+    camera_coords = depth * np.dot(K_inv, pixel_coords_homogeneous)
+
+    # The distance between camera and center of F1-10
+    offset_y = 0.3
+
+    return camera_coords[0], camera_coords[1] + offset_y, camera_coords[2]
